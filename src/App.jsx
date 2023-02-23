1147212bb222654bd96ae2dba7f62726bd97d61d
dallinforsyth <dallfor192@gmail.com>
2023-02-23 12:25:11 -0600

    created new post imput and 3 posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      </header>
+
+      <div id="posts-new">
+        <h1>New post</h1>
+        <label for="title"> Title: </label>
+        <input type="text" id="title" name="title" />
+        <label for="body"> Body: </label>
+        <input type="text" id="body" name="body" />
+        <label for="image"> Image: </label>
+        <input type="text" id="image" name="image" />
+        <div>
+          <input type="submit" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div id="posts-index">
+        <h1>All posts</h1>
+        <div>
+          <h2>Geese</h2>
+          <h5>Totally rad geese out there</h5>
+          <img src="https://www.disneyclips.com/images/images/goofy29.png" />
+        </div>
+        <div>
+          <h2>Apples</h2>
+          <h5>You get some of those apples form japan?</h5>
+          <img src="https://i.ebayimg.com/images/g/obEAAOSwCgleNsl8/s-l640.jpg" />
+        </div>
+        <div>
+          <h2>Rockets</h2>
+          <h5>Soon I want to fly on a rocket to the moon</h5>
+          <img src="https://imgix.bustle.com/uploads/image/2022/12/19/11ab3211-cadc-4b68-9c7a-e8ee149353e2-shutterstock_editorial_9224659b.jpg?w=1200&h=630&fit=crop&crop=focalpoint&fm=jpg&fp-x=0.42&fp-y=0.6127" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <footer>
+        <p>Copyright 20XX</p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
