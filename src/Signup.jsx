ddda1ded5b5c120053ab1fdff65b3e000aa1dc1c
dallinforsyth <dallfor192@gmail.com>
2023-02-27 14:09:10 -0600

    added signup button

---
+++ src/Signup.jsx
@@ -0,0 +1,21 @@
+export function Signup() {
+  return (
+    <div id="signup">
+      <h1>Signup</h1>
+      <form method="POST" action="http://localhost:3000/users.json" />
+      <div>
+        Name <input name="name" type="text" />
+      </div>
+      <div>
+        Email: <input name="email" type="email" />
+      </div>
+      <div>
+        Password: <input name="password" type="password" />
+      </div>
+      <div>
+        Password Confirmation: <input name="password_confirmation" type="password" />
+      </div>
+      <button type="submit">Signup</button>
+    </div>
+  );
+}
