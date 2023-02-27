7b58a25e8540c21f94c478d3efaf874fb25c8937
dallinforsyth <dallfor192@gmail.com>
2023-02-27 11:59:11 -0600

    added styling to posts with cards

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -5,11 +5,19 @@ export function PostsIndex(props) {
       <h1>All posts</h1>
       {props.posts.map((post) => {
         return (
-          <div className="posts" key={post.id}>
-            <h2>{post.title}</h2>
-            <h5>{post.body}</h5>
-            <img src={post.image} />
-            <button onClick={() => props.onShowPost(post)}>More info</button>
+          // <div className="posts" key={post.id}>
+          //   <h2>{post.title}</h2>
+          //   <h5>{post.body}</h5>
+          //   <img src={post.image} />
+          //   <button onClick={() => props.onShowPost(post)}>More info</button>
+          // </div>
+          <div className="card my-5">
+            <div className="card-body">
+              <h5 className="card-title">{post.title}</h5>
+              <p className="card-text">{post.body}</p>
+              <p className="card-text"></p>
+            </div>
+            <img src={post.image} className="card-img-bottom" alt="..." />
           </div>
         );
       })}
